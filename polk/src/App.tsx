e49779dac520ea79d9d2317ac01d5e5a73c70529
mylo <48256865+Milopadma@users.noreply.github.com>
2023-06-15 14:28:41 +0800

    figuring out the API because learning

--- polk/src/App.tsx
+++ polk/src/App.tsx
@@ -1,33 +1,80 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+// polkadot js
+import { ApiPromise, WsProvider } from "@polkadot/api";
+
+interface Data {
+  blockNumber: string;
+  hash: string;
+  chainName: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState<Data>({
+    blockNumber: "",
+    hash: "",
+    chainName: "",
+  });
+
+  // Construct
+  const wsProvider = new WsProvider("wss://rpc.polkadot.io");
+  const api = ApiPromise.create({ provider: wsProvider });
+
+  // Do something after the component is rendered, but only once
+  useEffect(() => {
+    api
+      .then(async (api) => {
+        setData({
+          blockNumber: api.genesisHash.toHex(),
+          hash: api.consts.babe.epochDuration.toHuman() as string,
+          chainName: (await api.rpc.system.chain()).toString(),
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(data);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header className="font-black text-3xl">Polkadot.JS</header>
+        {/* Give me a table consisting of the data fetched from the Polkadot Nodes */}
+        <table>
+          <thead>
+            <tr>
+              {/* dynamically display headers based on the type Data */}
+              {Object.keys(data).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {
+                // If data is not empty, then display the data
+                data && (
+                  <>
+                    <td>{data.hash}</td>
+                    <td>{data.blockNumber}</td>
+                    <td>{data.chainName}</td>
+                  </>
+                )
+              }
+              {
+                // If data is empty, then display loading
+                !data && (
+                  // Else, display loading
+                  <td>Loading...</td>
+                )
+              }
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
